efd8125aa39dea0b80a8db26bb6b45149614be2f
EvanHankham <ethankham@my.waketech.edu>
2025-04-07 23:15:13 -0400

    Shooting

--- LuauLuauMayhem/Assets/Evan_Scripts/Shooter.cs
+++ LuauLuauMayhem/Assets/Evan_Scripts/Shooter.cs
@@ -1,16 +1,39 @@
 using UnityEngine;
 
-public class Shooter : MonoBehaviour
+public class PlayerShooter : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public GameObject projectilePrefab;
+    public Transform shootPoint;
+    public float projectileSpeed = 20f;
+    public float fireCooldown = 0.25f;
+    public Camera playerCamera;
 
-    // Update is called once per frame
+    private float lastFireTime;
+
     void Update()
     {
-        
+        if (Input.GetMouseButton(0) && Time.time >= lastFireTime + fireCooldown)
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        if (projectilePrefab == null || shootPoint == null || playerCamera == null)
+            return;
+
+        Ray ray = playerCamera.ViewportPointToRay(new Vector3(0.5f, 0.5f)); // center of screen
+        Vector3 shootDirection = ray.direction;
+
+        GameObject projectile = Instantiate(projectilePrefab, shootPoint.position, Quaternion.LookRotation(shootDirection));
+        Rigidbody rb = projectile.GetComponent<Rigidbody>();
+
+        if (rb != null)
+        {
+            rb.linearVelocity = shootDirection * projectileSpeed;
+        }
+
+        lastFireTime = Time.time;
     }
 }
